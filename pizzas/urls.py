13b6af82d34cd429ad6117016ba85247ccd88d95
nihal tekin <nihalpolattekin@gmail.com>
2023-08-31 10:11:03 +0200

    first commit

---
+++ pizzas/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from .views import home
+
+urlpatterns = [
+    #tirnak icinde pathi beirtir daha sonra degisiklik yaparsam ileride diger kodlarlada da degisilik yapmma lazim. 
+    # Bu yüzden name verip kolayca geyebiliyorum degisebiliyorum. 
+   path('', home, name="home"),
+]
