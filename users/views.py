e69cd9bfd5174092d736ae2825ab7b6be99f939d
nihal tekin <nihalpolattekin@gmail.com>
2023-08-31 12:17:13 +0200

    login,logout

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .forms import UserForm
+
+
 
 
 # Create your views here.
@@ -10,7 +12,7 @@ def register(request):
     form=UserForm()
 
     if request.method=='POST':
-        form=UserCreationForm(request.POST)
+        form=UserForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect("home")
@@ -20,3 +22,22 @@ def register(request):
     }
 
     return render(request, 'users/register.html', context)
+
+
+from django.contrib.auth import login
+def user_login(request):
+    form=AuthenticationForm(data=request.POST)
+    if form.is_valid():
+        user=form.get_user()
+        login(request, user)
+        return redirect('home')
+    
+    context={
+        'form':form
+    }
+    return render(request, 'users/login.html', context)
+
+from django.contrib.auth import logout
+def user_logout(request):
+    logout(request)
+    return redirect('home')
