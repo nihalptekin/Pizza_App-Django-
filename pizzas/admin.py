3fc5aabd8c675b4dfda2f8a2d7a547aac5b6935e
nihal tekin <nihalpolattekin@gmail.com>
2023-09-01 11:12:19 +0200

    pizza.html,models

--- pizzas/admin.py
+++ pizzas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Toppings, Order, Pizza
 
 # Register your models here.
+admin.site.register(Toppings)
+admin.site.register(Order)
+admin.site.register(Pizza)
--- pizzas/views.py
+++ pizzas/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import Pizza
 
 # Create your views here.
 def home(request):
     context={
-
     }
     return render(request, 'pizzas/home.html', context)
+
+def pizzas(request):
+    pizzas=Pizza.objects.all()
+    context={
+        'pizzas': pizzas
+    }
+    return render(request, 'pizzas/pizzas.html', context)
